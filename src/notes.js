9cf41a95835da7d1ab2a37b8287badda5e112718
Alief Ahmad Azies <zies@Alief-Azies.local>
2022-04-14 10:45:38 +0700

    first commit

---
+++ src/notes.js
@@ -0,0 +1,4 @@
+const notes = [];
+const { nanoid } = require("nanoid");
+
+module.exports = notes;
---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editByNoteHandler,
+  deleteNoteByIdHandler,
+} = require("./handler");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editByNoteHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
